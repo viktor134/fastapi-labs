21213cd2078084e1671ef9f1b94e928ad172bb47
Victor <tricika96@gmail.com>
2025-08-31 05:39:32 -0700

    validate section

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/hotels")
-def get_hotels():
-    return "Hotel Ani resort"
+@app.get("/hotels/{hotel_id}")
+def get_hotels(hotel_id: int,date_from,date_to):
+    return hotel_id , date_from ,date_to
+
 
