9becd1e64905b8096de423b5c0d30a52861fe080
Victor <tricika96@gmail.com>
2025-09-01 09:59:23 -0700

    add post request

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI,Query
 from typing import Optional
 from datetime import date
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+class SHotel(BaseModel):
+    address:str
+    name:str
+    stars:int
+
+
+
 
 
 @app.get("/hotels")
@@ -13,7 +22,25 @@ def get_hotels(
     date_to :date,
     stars:Optional[int] = Query (None , ge =1,le=5),
     has_spa:Optional[bool] = None,
-):
-    return date_from ,date_to
+) ->list[SHotel]:
+    hotels = [
+        {
+            "addreess": "1610 Main Street",
+            "name": "Super Hotel",
+            "stars": 5 ,
+            
+        },
+    ]
 
+    
+    return hotels
 
+class SBoking(BaseModel):
+    root_id : int
+    date_from: date 
+    date_to: date
+
+@app.post("/bookings") 
+def add_booking(booking:SBoking):
+    pass
+ 
