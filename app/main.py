59dcc9b76cf714936d33bcd8d6b0853d50226c37
Victor <tricika96@gmail.com>
2025-09-01 10:14:41 -0700

    edit code create new cclass

--- app/main.py
+++ app/main.py
@@ -1,39 +1,38 @@
-from fastapi import FastAPI,Query
+from fastapi import FastAPI,Query,Depends
 from typing import Optional
 from datetime import date
 from pydantic import BaseModel
 
 
+
 app = FastAPI()
 
-class SHotel(BaseModel):
-    address:str
-    name:str
-    stars:int
 
+class HotelsSearchArgs:
+    def __init__(
+        self,
+        location:str,
+        date_from:date,
+        date_to :date,
+        stars:Optional[int] = Query (None , ge =1,le=5),
+        has_spa:Optional[bool] = None,
 
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
 
 
 
 @app.get("/hotels")
 def get_hotels(
-    location:str,
-    date_from:date,
-    date_to :date,
-    stars:Optional[int] = Query (None , ge =1,le=5),
-    has_spa:Optional[bool] = None,
-) ->list[SHotel]:
-    hotels = [
-        {
-            "addreess": "1610 Main Street",
-            "name": "Super Hotel",
-            "stars": 5 ,
-            
-        },
-    ]
+    search_args:HotelsSearchArgs = Depends()
+):
 
     
-    return hotels
+    return search_args
 
 class SBoking(BaseModel):
     root_id : int
